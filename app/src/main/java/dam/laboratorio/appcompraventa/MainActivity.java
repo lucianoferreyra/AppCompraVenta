4ca9c8c54b41bfadac7e2c88ce7ad7621fdb5415
Juan Diego Paduli <paduli.18@gmail.com>
2021-09-22 18:34:55 -0300

    Se agregan validaciones del checkbox "retiro a domicilio" y "acepto terminos y condiciones" Se realizaron validaciones en la visibilidad de los campos de direccion a domicilio y de terminos y condiciones.

--- app/src/main/java/dam/laboratorio/appcompraventa/MainActivity.java
+++ app/src/main/java/dam/laboratorio/appcompraventa/MainActivity.java
@@ -2,8 +2,15 @@ package dam.laboratorio.appcompraventa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +31,50 @@ public class MainActivity extends AppCompatActivity {
 
         // Se aplica el adapter al spinner
         cat_spinner.setAdapter(adapter);
+
+        //Obtengo los valores de la view para trabajar con las variables de "retiro" y "terminos y condiciones"
+        CheckBox ch1 = (CheckBox) findViewById(R.id.checkRetiro);
+        CheckBox ch2 = (CheckBox) findViewById(R.id.checkTermino);
+        TextView tv1 = (TextView) findViewById(R.id.textViewRetiro);
+        EditText ed1 = (EditText) findViewById(R.id.editTextTextRetiro);
+
+
+
+        //Verifico el checkbox de retiro a domicilio para hacer visible el campo domicilio
+        ch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    tv1.setVisibility(View.VISIBLE);
+                    ed1.setVisibility(View.VISIBLE);
+                }
+                else{
+                    tv1.setVisibility(View.GONE);
+                    ed1.setVisibility(View.GONE);
+                }
+            }
+
+        });
+
+        //Obtengo el valor del toggle "Publicar"
+        Button btn1 = (Button) findViewById(R.id.ButtonPublicar);
+
+        //Verifico la validacion del checkbox "Terminos y condiciones" para habilitar el Toggle "Publicar"
+        ch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    btn1.setEnabled(true);
+                }
+                else{
+                    btn1.setEnabled(false);
+
+                }
+            }
+
+        });
+
     }
+
+
 }
